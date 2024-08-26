792f0c3f1f6dacf0b91fd30668398c4ae580a971
ducanhlo <liamloducanh@gmail.com>
2024-08-26 19:54:35 +0100

    Initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
 
-import TodoDash from '@/components/TodoDash'
 import { Suspense } from 'react'
 
 
 export default function Home() {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <TodoDash />
-    </Suspense>
+   <h1>Home</h1>
   )
 }
